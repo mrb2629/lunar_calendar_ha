8eba4f33e0203f2f1caeabbf2bfd98df26607a0a
mrb2629 <mrb2629@gmail.com>
2025-09-19 01:25:07 +0700

    Update __init__.py

--- custom_components/lunar_calendar/__init__.py
+++ custom_components/lunar_calendar/__init__.py
@@ -1,18 +1,18 @@
 """The Lunar Calendar integration."""
 DOMAIN = "lunar_calendar"
-PLATFORMS = ["sensor"] # Khai báo các nền tảng sẽ setup
+PLATFORMS = ["sensor"]
 
 async def async_setup(hass, config):
     """Set up the Lunar Calendar component."""
-    return True # Không làm gì nếu không có config trong configuration.yaml
+    return True
 
+# HÀM NÀY PHẢI GỌI setup_entry của nền tảng Sensor
 async def async_setup_entry(hass, entry):
     """Set up Lunar Calendar from a config entry."""
-    # Gọi hàm setup_platform bất đồng bộ cho tất cả các nền tảng
+    # SỬ DỤNG 'async_forward_entry_setups' (số nhiều)
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass, entry):
     """Unload a config entry."""
-    # Gọi hàm unload_platform bất đồng bộ
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
